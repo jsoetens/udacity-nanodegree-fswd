eebd6e77686fe0f2a8046c40a393a868c6724d55
Soetens, Johan <Johan.Soetens@lidl.be>
2017-08-16 12:47:58 +0200

    style: fix comments to be pep8 compliant

--- fswd-project-3/logs_analysis.py
+++ fswd-project-3/logs_analysis.py
@@ -7,7 +7,7 @@ class LogAnalyzer(object):
 
     """
     This class connects to a PostgreSQL database called news and provides
-    reporting on the site's articles. It does that by answering 3 questions.
+    reporting on the site's articles. It does that by answering some questions.
     """
     def __init__(self, dbname):
         self.dbname = dbname
@@ -21,22 +21,29 @@ class LogAnalyzer(object):
         return self._db_cur.execute(query)
 
     def get_popular_articles(self, limit):
-        # What are the most popular three articles of all time?
-        # "Princess Shellfish Marries Prince Handsome" — 1201 views
+        """
+        What are the most popular three articles of all time?
+        Example: "Princess Shellfish Marries Prince Handsome" — 1201 views
+        Requires view v_log_articles.
+        """
         query = """
             select article_title, total_views
             from v_log_articles
             order by total_views desc
             limit {0};
         """.format(limit)
-        print("These are the {0} most popular articles of all time:\n".format(limit))
+        print("These are the {0} most popular articles of \
+            all time:\n".format(limit))
         self.query(query)
         for record in self._db_cur:
             print("\"{0}\" - {1} views".format(record[0], record[1]))
 
     def get_popular_authors(self):
-        # Who are the most popular article authors of all time?
-        # Ursula La Multa — 2304 views
+        """
+        Who are the most popular article authors of all time?
+        Example: Ursula La Multa — 2304 views
+        Requires view v_log_articles.
+        """
         query = """
             select author_name, sum(total_views) as total_views
             from v_log_articles
@@ -47,9 +54,13 @@ class LogAnalyzer(object):
         print("\nThese are the most popular article authors of all time:\n")
         for record in self._db_cur:
             print("{0} - {1} views".format(record[0], record[1]))
+
     def get_days_with_errors(self, percentage):
-        # On which days did more than 1% of requests lead to errors?
-        # July 29, 2016 — 2.5% errors
+        """
+        On which days did more than 1% of requests lead to errors?
+        Example: July 29, 2016 — 2.5% errors
+        Requires view v_log_errors.
+        """
         query = """
             select to_char(log_date, 'Mon DD, YYYY'), percentage
             from v_log_errors
@@ -62,8 +73,10 @@ class LogAnalyzer(object):
             print("{0} - {1}% errors".format(record[0], record[1]))
 
     def __del__(self):
-        # Destructor: called when the instance is about to be destroyed.
-        # close the connection
+        """
+        Destructor: called when the instance is about to be destroyed.
+        Closes the the connection
+        """
         self._db_connection.close()
 
 
@@ -72,7 +85,6 @@ def main():
     report.get_popular_articles(3)
     report.get_popular_authors()
     report.get_days_with_errors(1)
-
 
 if __name__ == '__main__':
     main()
